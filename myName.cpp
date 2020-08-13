830862c18b0d765f6b6dce64e429c111529d6df2
PeterCrosta <pscrosta@gmail.com>
2020-08-13 08:13:51 -0400

    dumb name program

---
+++ myName.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string myName;
+    std::cout << "What's my name?";
+    std::cin >> myName;
+    std::cout << "That's right, motherfucker! I'm " << myName << ", and don't you forget it!\n";
+}
